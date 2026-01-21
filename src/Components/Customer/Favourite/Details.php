d0462cdf05e428a78092d27874757c41e4f5c2ed
hafijul1996 <hafijul1996@users.noreply.github.com>
2026-01-21 22:50:18 +0000

    ACP-135 other: fixing php code style issues

--- src/Components/Customer/Favourite/Details.php
+++ src/Components/Customer/Favourite/Details.php
@@ -23,7 +23,7 @@ class Details extends BaseComponent
     public function render(): View|Closure|string
     {
         $param = request()->route('favourite');
-        if (!is_numeric($param)) {
+        if (! is_numeric($param)) {
             abort(404, 'Page Not Found');
         }
 
