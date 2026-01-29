5f8913e635fb5753a8de27f4f2a90d897b363ba7
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-01-30 04:08:58 +0600

    :pencil2: added a extra meta tags for seo

--- src/Components/Favicon.php
+++ src/Components/Favicon.php
@@ -12,27 +12,6 @@ use Illuminate\Contracts\View\View;
 class Favicon extends BaseComponent
 {
     /**
-     * @var array
-     */
-    public $options;
-
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
-     * Whether the component should be rendered
-     */
-    public function shouldRender(): bool
-    {
-        return true;
-    }
-
-    /**
      * Get the view / contents that represent the component.
      */
     public function render(): View|Closure|string
