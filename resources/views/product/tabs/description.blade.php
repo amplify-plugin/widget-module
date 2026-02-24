ccdc33b6209c9240f059872d1093ca3f11a53927
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-02-24 14:31:15 +0600

    :bug: empty description tab hide option added

--- resources/views/product/tabs/description.blade.php
+++ resources/views/product/tabs/description.blade.php
@@ -1,14 +1,16 @@
-@push('title')
-    <li class="nav-item">
-        <a class="nav-link" data-toggle="tab" href="#{{ $entry['name'] }}" role="tab">
-            {{ __($entry['label']) }}
-        </a>
-    </li>
-@endpush
+@if(!empty($product->description))
+    @push('title')
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#{{ $entry['name'] }}" role="tab">
+                {{ __($entry['label']) }}
+            </a>
+        </li>
+    @endpush
 
-@push('content')
-    <div class="tab-pane fade" id="{{ $entry['name'] }}" role="tabpanel"
-         aria-labelledby="{{ $entry['name'] }}">
-        {!! $product->description !!}
-    </div>
-@endpush
+    @push('content')
+        <div class="tab-pane fade" id="{{ $entry['name'] }}" role="tabpanel"
+             aria-labelledby="{{ $entry['name'] }}">
+            {!! $product->description !!}
+        </div>
+    @endpush
+@endif
